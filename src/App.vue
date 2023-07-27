97180afcebff52cb00edf2a01f913497f065e0ec
Ulakhovich Katsiaryna <e.ulahovich@belitsoft.com>
2023-07-27 14:48:04 +0300

    all

---
+++ src/App.vue
@@ -0,0 +1,114 @@
+<template>
+  <header>
+    <h1>Wardrobe</h1>
+  </header>
+  <main>
+    <div class="page">
+      <div class="section left">
+        <slider :data="dataFull"/>
+      </div>
+      <div class="center">
+        <div class="section coats">
+          <slider :data="dataCoats"/>
+        </div>
+        <div class="section pants">
+          <slider :data="dataPants"/>
+        </div>
+      </div>
+      <div class="right">
+        <div class="section hats">
+          <slider :data="dataHats"/>
+        </div>
+        <div class="section bags">
+          <slider :data="dataBags"/>
+        </div>
+        <div class="section shoes">
+          <slider :data="dataShoes"/>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script setup>
+  import Slider from "./components/Slider.vue";
+  import { ref } from "vue";
+
+  const dataCoats = ref([
+      'src/assets/cloth/coats/1674046687-bestwhiteshirts2023-mango-1674046465.png',
+      'src/assets/cloth/coats/hmgoepprod.jpg',
+      'src/assets/cloth/coats/sportswear-phoenix-fleece-womens-over-oversized-crewneck-sweatshirt-Wj2Rd6.png'
+  ])
+  const dataBags = ref([
+      "src/assets/cloth/bags/2022-L6-CK2-20151158-01-1.jpg",
+      "src/assets/cloth/bags/148605A213_main.jpg"
+  ])
+  const dataHats = ref([
+      'src/assets/cloth/hats/Chok.Lids Everyday Bucket Hat.webp',
+      'src/assets/cloth/hats/eb0cb2f7-350f-49ea-95f9-86a87207a32e.webp'
+  ])
+  const dataPants = ref([
+      'src/assets/cloth/pants/100140170113000_0_1664227209494.jpg',
+      'src/assets/cloth/pants/h_5051839610620.jpg'
+  ])
+  const dataFull = ref([
+      'src/assets/cloth/full/1656012267-best-flowy-dresses-women-doen-dress-1656012229.png',
+      'src/assets/cloth/full/best.jpg'
+  ])
+  const dataShoes = ref([
+      'src/assets/cloth/shoes/1433977020_main.jpg',
+      'src/assets/cloth/shoes/images.jpg'
+  ])
+</script>
+
+<style>
+  .page {
+    display: flex;
+    box-sizing: border-box;
+    height: 95vh;
+    gap: 10px;
+  }
+
+  .left {
+    max-width: 30%;
+  }
+
+  .center {
+    max-width: 40%;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+  }
+
+  .right {
+    max-width: 30%;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+  }
+
+  .section {
+    width: 100%;
+  }
+
+  .coats {
+    height: 43vh;
+  }
+
+  .pants {
+    height: 50vh;
+  }
+
+
+  .hats {
+    height: 30vh;
+  }
+
+  .bags {
+    height: 31vh;
+  }
+
+  .shoes {
+    height: 31vh;
+  }
+</style>
---
+++ src/components/Slider.vue
@@ -0,0 +1,86 @@
+<template>
+  <swiper :pagination="pagination" :loop="true" :navigation="true" :modules="modules" class="mySwiper">
+    <swiper-slide v-for="(item, index) in items.data">
+      <img :src="item" :alt="index">
+    </swiper-slide>
+    <swiper-slide>
+      <div class="empty-slide"></div>
+    </swiper-slide>
+  </swiper>
+</template>
+
+<script setup>
+import { Pagination, Navigation } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/vue'
+
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const items = defineProps(['data'])
+
+const pagination = {
+  clickable: true,
+  renderBullet: function (index, className) {
+    return '<span class="' + className + '">' + (index + 1) + '</span>';
+  },
+}
+
+const modules = [Pagination, Navigation];
+
+</script>
+
+<style>
+.swiper {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+}
+
+.swiper-slide {
+  text-align: center;
+}
+
+.swiper-slide img {
+  display: block;
+  max-height: 100%;
+  max-width: 100%;
+  margin: auto;
+}
+
+.swiper:hover .swiper-pagination-bullet {
+  opacity: 1;
+}
+
+.swiper-pagination-bullet {
+  width: 20px;
+  height: 20px;
+  text-align: center;
+  line-height: 20px;
+  font-size: 12px;
+  opacity: 0;
+  background: rgba(0, 0, 0, 0.2);
+}
+
+.swiper-pagination-bullet-active {
+  background: #a1be94;
+}
+
+.swiper-button-prev,
+.swiper-button-next {
+  opacity: 0;
+  color: #a1be94;
+}
+
+.swiper:hover .swiper-button-prev,
+.swiper:hover .swiper-button-next {
+  opacity: 1;
+}
+
+.empty-slide {
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+}
+</style>
