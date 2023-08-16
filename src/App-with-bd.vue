000fac1e0bdc3da9686fe1de1ee24eb55134907c
Katsiaryna <36642433+UlakhovichKate@users.noreply.github.com>
2023-08-16 15:49:38 +0300

    with local files

---
+++ src/App-with-bd.vue
@@ -0,0 +1,103 @@
+<template>
+  <header class="header">
+    <h1>Wardrobe</h1>
+    <filters @select-tag="getFilter" />
+  </header>
+  <main>
+    <div class="page">
+      <div class="section left">
+        <slider :selectedTag="selectedTag" data="9"/>
+      </div>
+      <div class="center">
+        <div class="section coats">
+          <slider :selectedTag="selectedTag" data="10"/>
+        </div>
+        <div class="section pants">
+          <slider :selectedTag="selectedTag" data="11"/>
+        </div>
+      </div>
+      <div class="right">
+        <div class="section hats">
+          <slider :selectedTag="selectedTag" data="12"/>
+        </div>
+        <div class="section bags">
+          <slider :selectedTag="selectedTag" data="18"/>
+        </div>
+        <div class="section shoes">
+          <slider :selectedTag="selectedTag" data="13"/>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script setup>
+  import Slider from "./components/Slider.vue";
+  import {ref} from "vue";
+  import Filters from "./components/Filters.vue";
+
+  const selectedTag = ref(null);
+
+  function getFilter(n) {
+    selectedTag.value = n;
+  }
+
+</script>
+
+<style>
+ .header {
+   display: flex;
+   gap: 10px;
+   padding: 10px 0 10px;
+ }
+
+  .page {
+    display: flex;
+    box-sizing: border-box;
+    height: 95vh;
+    gap: 10px;
+  }
+
+  .left {
+    max-width: 30%;
+  }
+
+  .center {
+    max-width: 40%;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+  }
+
+  .right {
+    max-width: 30%;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+  }
+
+  .section {
+    width: 100%;
+  }
+
+  .coats {
+    height: 43vh;
+  }
+
+  .pants {
+    height: 50vh;
+  }
+
+
+  .hats {
+    height: 30vh;
+  }
+
+  .bags {
+    height: 31vh;
+  }
+
+  .shoes {
+    height: 31vh;
+  }
+</style>
--- src/components/Slider-with-bd.vue
+++ src/components/Slider-with-bd.vue
@@ -1,7 +1,8 @@
 <template>
   <swiper :pagination="pagination" :loop="true" :navigation="true" :modules="modules" class="mySwiper">
-    <swiper-slide v-for="(item, index) in items.data.data">
-      <img :src="`src/assets/cloth/${items.data.type}/${item}`" :alt="index">
+    <swiper-slide v-for="(item, index) in filteredItems" :key="index" class="slide">
+      <img :src="item.media" :alt="index">
+      <span class="tags">{{ item.tags }}</span>
     </swiper-slide>
     <swiper-slide>
       <div class="empty-slide"></div>
@@ -12,13 +13,52 @@
 <script setup>
 import { Pagination, Navigation } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/vue'
-
-// import Swiper and modules styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import {ref, watch, computed } from "vue";
 
-const items = defineProps(['data'])
+const props = defineProps(['data', 'selectedTag'])
+
+const items = ref(null);
+const filteredItems = ref(null);
+
+async function fetchBackend(category) {
+  try {
+    const response = await fetch('http://localhost:8000/wp-json/wp/v2/posts?categories='+ category +'&per_page=100');
+    const clothes = await response.json();
+
+    items.value = clothes.map(x => {
+          return {
+            'tags': x.tags,
+            'media': x.fimg_url
+          }
+        })
+    filteredItems.value = items.value
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+fetchBackend(props.data);
+
+watch( ()=> props.selectedTag, (newVal)=> {
+  filteredItems.value = items.value
+
+  if (newVal?.value.length > 0) {
+    filteredItems.value = filteredItems.value.filter(x => {
+      const found = x.tags.some(r=> newVal.value.indexOf(r) >= 0)
+      if (found) {
+        return x
+      }
+    })
+
+  } else {
+    filteredItems.value = items.value
+  }
+
+}, {immediate:true, deep: true});
 
 const pagination = {
   clickable: true,
@@ -85,5 +125,15 @@ const modules = [Pagination, Navigation];
   width: 100%;
   height: 100%;
   background-color: #fff;
+}
+
+.slide {
+  position: relative;
+}
+
+.tags {
+  position: absolute;
+  top: 50px;
+  left: 50px;
 }
 </style>
